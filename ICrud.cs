3722519ac7910d111ce6f4b81e497de0492d1698
OB1ADAET <116683120+OB1ADAET@users.noreply.github.com>
2023-02-08 23:31:26 +0300

    Add files via upload

---
+++ ICrud.cs
@@ -0,0 +1,13 @@
+﻿namespace prak10
+{
+    internal interface ICrud
+    {
+        void Create(); 
+        void Visualization();
+        void Update(); 
+        void Delete(); 
+        void Search(); 
+        void More(); 
+    }
+}
+
